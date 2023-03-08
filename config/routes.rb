8eb5be74bf5151e9be558f0cf9ed6e6bfe8c0286
Viet Tran <viettran101294@gmail.com>
2023-03-08 01:31:02 -0500

    admin category feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root to: 'products#index'
   resources :about, only: [:index]
   resources :products, only: [:index, :show]
@@ -15,6 +14,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
