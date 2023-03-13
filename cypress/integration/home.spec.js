cbe392a26d1f43662333a847cfef5aa46c9efc72
Viet Tran <viettran101294@gmail.com>
2023-03-13 18:31:52 -0400

    compelted homepage cypress test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+});
