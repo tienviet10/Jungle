62d3872c2a30012bcd5e03a0dc1a348b4d1ad3e8
Viet Tran <viettran101294@gmail.com>
2023-03-13 16:27:53 -0400

    completed user spec testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, presence: true, length: { minimum:3 }, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  
+
+  def self.authenticate_with_credentials(email, password)
+      clean_email = email.downcase.strip
+      # user = User.find_by_email(clean_email)
+      user = User.where('lower(email) = ?', clean_email).first
+      
+      if user && user.authenticate(password)
+        user
+      else
+        nil
+      end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.new(:email => "m@gmail.com", :name => "Mary", :password => "12345", :password_confirmation => "12345") }
+
+  describe 'Validations' do
+    it 'is not valid without password' do
+      user.password = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without password_confirmation' do
+      user.password_confirmation = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'confirm password and password_confirmation are not matching' do
+      user.password_confirmation = "4231"
+      user.save
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'confirm password and password_confirmation are not matching' do
+      user.save
+      expect(user.name).to eql("Mary")
+    end
+
+    it 'is not valid when email is not unique' do
+      user.save
+      user2 = User.new(:email => "M@gmail.com", :name => "Mary", :password => "12345", :password_confirmation => "12345")
+      expect(user2).not_to be_valid
+    end
+
+    it 'confirm email should be required' do
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'confirm name should be required' do
+      user.name = nil
+      user.save
+      expect(user).not_to be_valid
+      expect(user.errors.full_messages).to include("Name can't be blank")
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it 'return nil when failed to authenticate' do
+      user.save
+      user = User.authenticate_with_credentials('m@gmail.com', '123485')
+      expect(user).to eql(nil)
+    end
+
+    it 'return correct email when corectly authenticated' do
+      user.save
+      user = User.authenticate_with_credentials('m@gmail.com', '12345')
+      expect(user).not_to eql(nil)
+    end
+
+    it 'can authenticate with case sensitive email address' do
+      user.save
+      user = User.authenticate_with_credentials('M@gmail.com', '12345')
+      expect(user.email).to eql('m@gmail.com')
+      expect(user).not_to eql(nil)
+    end
+  end
+
 end
